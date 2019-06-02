3b7359a9511728317336a3b78117dc0274c11d58
Brent Gardner <bgardner@squarelabs.net>
2019-06-02 16:15:31 -0600

    Can get language details

--- assets/js/pages/language-edit.tsx
+++ assets/js/pages/language-edit.tsx
@@ -3,17 +3,35 @@ import {Link, Redirect} from 'react-router-dom';
 
 import Main from '../components/Main';
 
-interface ExampleState {
+interface Language {
+    id: number;
+    name: string;
+    proverb: string;
 }
 
-export default class LanguageEditPage extends React.Component<
-    {},
-    ExampleState
-    > {
+interface ApiResponse {
+    data: Language
+}
+
+interface ExampleState {
+    language: Language,
+    loading: boolean
+}
+
+export default class LanguageEditPage extends React.Component<any, ExampleState> {
     constructor(props: {}) {
         super(props);
-        this.state = {};
+        this.state = {language: {id: 0, name: '', proverb: ''}, loading: true};
+        this.loadData(this.props.match.params.id)
     }
+
+    loadData = (id: any) => {
+        fetch(`/api/languages/${id}`)
+            .then(response => response.json() as Promise<ApiResponse>)
+            .then(data => {
+                this.setState({ language: data.data, loading: false });
+            });
+    };
 
     public render(): JSX.Element {
         return (
@@ -21,11 +39,11 @@ export default class LanguageEditPage extends React.Component<
                 <h1>Edit Language</h1>
                 <p>
                     <label htmlFor="name">Name</label>
-                    <input id="name" type="text"/>
+                    <input id="name" type="text" value={this.state.language.name}/>
                 </p>
                 <p>
                     <label htmlFor="proverb">Proverb</label>
-                    <input id="proverb" type="text"/>
+                    <input id="proverb" type="text" value={this.state.language.proverb}/>
                 </p>
                 <button>Save</button>
             </Main>
