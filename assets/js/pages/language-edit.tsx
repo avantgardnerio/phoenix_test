69d3da20ccd610ab70834703ec2180b155cbab5c
Brent Gardner <bgardner@squarelabs.net>
2019-06-02 17:25:09 -0600

    Update

--- assets/js/pages/language-edit.tsx
+++ assets/js/pages/language-edit.tsx
@@ -15,13 +15,14 @@ interface ApiResponse {
 
 interface ExampleState {
     language: Language,
-    loading: boolean
+    loading: boolean,
+    saved: boolean
 }
 
 export default class LanguageEditPage extends React.Component<any, ExampleState> {
     constructor(props: {}) {
         super(props);
-        this.state = {language: {id: 0, name: '', proverb: ''}, loading: true};
+        this.state = {language: {id: 0, name: '', proverb: ''}, loading: true, saved: false};
         this.loadData(this.props.match.params.id)
     }
 
@@ -33,19 +34,54 @@ export default class LanguageEditPage extends React.Component<any, ExampleState>
             });
     };
 
+    private change = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(this.state);
+        const newState = {
+            language: {...this.state.language, [e.target.id]: e.target.value}
+        };
+        this.setState(newState)
+    };
+
+    private save = () => {
+        const json = JSON.stringify({
+            id: this.state.language.id,
+            language: {
+                name: this.state.language.name,
+                proverb: this.state.language.proverb
+            }
+        });
+        const options = {
+            method: 'put',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: json
+        };
+        fetch(`/api/languages/${this.state.language.id}`, options)
+            .then(res => res.json())
+            .then(res => {
+                // TODO: check result
+                this.setState({...this.state, saved: true});
+            });
+    };
+
     public render(): JSX.Element {
+        if(this.state.saved) {
+            return <Redirect to='/fetch-data' />;
+        }
         return (
             <Main>
                 <h1>Edit Language</h1>
                 <p>
                     <label htmlFor="name">Name</label>
-                    <input id="name" type="text" value={this.state.language.name}/>
+                    <input id="name" type="text" onChange={this.change} value={this.state.language.name}/>
                 </p>
                 <p>
                     <label htmlFor="proverb">Proverb</label>
-                    <input id="proverb" type="text" value={this.state.language.proverb}/>
+                    <input id="proverb" type="text" onChange={this.change} value={this.state.language.proverb}/>
                 </p>
-                <button>Save</button>
+                <button onClick={this.save}>Save</button>
             </Main>
         );
     }
